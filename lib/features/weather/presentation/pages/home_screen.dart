28d70e469b81fc66f88c5c2ad83c28a0bb623826
moshated <mobin.cofingfluter@gmail.com>
2023-02-01 14:12:17 +0330

    pageview builder done

--- lib/features/weather/presentation/pages/home_screen.dart
+++ lib/features/weather/presentation/pages/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:clean_architecture_forcast_weather/core/presentation/widgets/dot_loading.dart';
 import 'package:clean_architecture_forcast_weather/features/weather/presentation/bloc/weather_bloc.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -21,37 +22,39 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    BlocProvider.of<WeatherBloc>(context).add(LoadCwEvent('Sanandaj'));
+    BlocProvider.of<WeatherBloc>(context).add(LoadCwEvent('Tehran'));
   }
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-    final PageController _controller = PageController();
+    final PageController _controller = PageController(keepPage: true);
     return SafeArea(
       child: Column(
         children: [
-          Expanded(
-            child: BlocBuilder<WeatherBloc, WeatherState>(
-              builder: (context, state) {
-                if (state.cwStatus is CwLoading) {
-                  return const Expanded(child: DotLoadingWidget());
-                }
-                if (state.cwStatus is CwComplited) {
-                  final CwComplited cwComplited = state.cwStatus as CwComplited;
-                  final CurrentCityEntity currentCityEntity =
-                      cwComplited.currentCityEntity;
+          BlocBuilder<WeatherBloc, WeatherState>(
+            builder: (context, state) {
+              if (state.cwStatus is CwLoading) {
+                return const Expanded(child: DotLoadingWidget());
+              }
+              if (state.cwStatus is CwComplited) {
+                final CwComplited cwComplited = state.cwStatus as CwComplited;
+                final CurrentCityEntity currentCityEntity =
+                    cwComplited.currentCityEntity;
 
-                  return Expanded(
-                    child: ListView(
-                      shrinkWrap: true,
-                      children: [
-                        PageView.builder(
-                          physics: const AlwaysScrollableScrollPhysics(),
+                return Expanded(
+                  child: ListView(
+                    children: [
+                      SizedBox(
+                        height: 400,
+                        width: 400,
+                        child: PageView.builder(
+                          physics: AlwaysScrollableScrollPhysics(),
                           allowImplicitScrolling: true,
                           controller: _controller,
                           itemCount: 2,
+                          pageSnapping: true,
                           itemBuilder: (context, index) {
                             if (index == 0) {
                               return Column(
@@ -167,43 +170,43 @@ class _HomeScreenState extends State<HomeScreen> {
                             return Text('data');
                           },
                         ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Center(
-                          child: SmoothPageIndicator(
-                            controller: _controller,
-                            count: 2,
-                            effect: ExpandingDotsEffect(
-                              dotColor: Colors.white,
-                              dotHeight: 10,
-                              dotWidth: 10,
-                              spacing: 5,
-                            ),
-                            onDotClicked: (index) => _controller.animateToPage(
-                              index,
-                              duration: Duration(milliseconds: 300),
-                              curve: Curves.easeIn,
-                            ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                        child: SmoothPageIndicator(
+                          controller: _controller,
+                          count: 2,
+                          effect: ExpandingDotsEffect(
+                            dotColor: Colors.white,
+                            dotHeight: 10,
+                            dotWidth: 10,
+                            spacing: 5,
+                          ),
+                          onDotClicked: (index) => _controller.animateToPage(
+                            index,
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.easeIn,
                           ),
                         ),
-                      ],
-                    ),
-                  );
-                }
-                if (state.cwStatus is CwError) {
-                  const Center(
-                    child: Text('error'),
-                  );
-                }
-                return const Center(
-                  child: Text(
-                    'nothing',
-                    style: TextStyle(color: Colors.white),
+                      ),
+                    ],
                   ),
                 );
-              },
-            ),
+              }
+              if (state.cwStatus is CwError) {
+                const Center(
+                  child: Text('error'),
+                );
+              }
+              return const Center(
+                child: Text(
+                  'nothing',
+                  style: TextStyle(color: Colors.white),
+                ),
+              );
+            },
           ),
         ],
       ),
